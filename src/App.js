8ea78cbe1c74737e4675b08ca3b386764a971114
jgutierrezre <jgutierrezre@unal.edu.co>
2022-03-19 09:22:56 -0500

    finished adding notes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { BrowserRouter, Routes, Route, Navigate, Outlet } from 'react-router-dom';
+import uuid from 'react-uuid';
 
 import Login from './pages/Login'
 import Secret from './pages/Secret'
@@ -10,13 +11,52 @@ import Main from './components/Main';
 
 class App extends Component {
 
+	constructor(props) {
+		super(props);
+		this.state = {
+		  notes: []
+		};
+	  }
+
+	onAddNote = () => {
+		const newNote = {
+			id: uuid(),
+			title: "Untitled Note",
+			body: "",
+			lastModified: Date.now(),
+		};
+		this.setState({ notes: [newNote, ...this.state.notes] });
+	}
+
+	onDeleteNote = (idToDelete) => {
+		this.setState({ notes: this.state.notes.filter((note) => note.id !== idToDelete) });
+	}
+
+	IsLogged = () => {
+		const auth = true;
+		return auth ? <Outlet/> : <Navigate to="/login"/>;
+	}
+
+	Default = () => {
+		return (
+			<>
+				<Sidebar 
+					notes={this.state.notes} 
+					onAddNote={this.onAddNote}
+					onDeleteNote={this.onDeleteNote}
+				/>
+				<Main/>
+			</>
+		)
+	}
+
 	render() {
 		return (
 			<div className='App'>
 				<BrowserRouter>
 					<Routes>
-						<Route path='/' element={<IsLogged/>}>
-							<Route path='/' element={<Default/>}/>
+						<Route path='/' element={<this.IsLogged/>}>
+							<Route path='/' element={<this.Default/>}/>
 						</Route>
 						<Route path='/login' element={<Login/>}/>
 					</Routes>
@@ -24,20 +64,6 @@ class App extends Component {
 			</div>
 		);
 	}
-}
-
-const IsLogged = () => {
-    const auth = true;
-    return auth ? <Outlet/> : <Navigate to="/login"/>;
-}
-
-const Default = () => {
-	return (
-		<>
-			<Sidebar/>
-			<Main/>
-		</>
-	)
 }
 
 export default App;
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,23 +1,30 @@
+import { notStrictEqual } from "assert";
 import React, { Component } from "react";
 import {Link} from 'react-router-dom';
 
 class Sidebar extends Component {
     render() {
+        console.log(this.props.notes)
 		return (
             <div className="app-sidebar">
                 <div className="app-sidebar-header">
                     <h1>Calpad</h1>
-                    <button>Add</button>
+                    <button onClick={this.props.onAddNote}>Add</button>
                 </div>
                 <div className="app-sidebar-notes">
-                    <div className="app-sidebar-note">
-                        <div className="sidebar-note-title">
-                            <strong>TITLE</strong>
-                            <button>Delete</button>
+                    {this.props.notes.map((note) => (
+                        <div className="app-sidebar-note" key={note.id}>
+                            <div className="sidebar-note-title">
+                                <strong>{note.title}</strong>
+                                <button onClick={() => this.props.onDeleteNote(note.id)}>Delete</button>
+                            </div>
+                            <p>{note.body && note.body.substring(0,100) + "..."}</p>
+                            <small className="note-meta">Last modified {new Date(note.lastModified).toLocaleDateString("en-US", {
+                                hour: "2-digit",
+                                minute: "2-digit",
+                            })}</small>
                         </div>
-                        <p>Note preview.</p>
-                        <small className="note-meta">Last modified [date]</small>
-                    </div>
+                    ))}
                 </div>
                 <Link to="/">
                     <button>
